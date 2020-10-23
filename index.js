2a573081fe05a6598834c0aa83c260d06a50c1b2
genie <wlsgml5628@gmail.com>
2020-10-23 22:16:14 +0900

    codespitz78_6 - TodoApp 리팩토링

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const err = v => {
 }
 
 const Task = class{
+    static get(title) {return new Task (title);} // Factory pattern
     constructor(title, isCompleted = false) {
         this.title = title; // mutable
         this.isCompleted = isCompleted; // status를 Boolean으로 관리 (비추천) --> 열거값 or 참조형 사용으로 대체
@@ -35,72 +36,54 @@ const Task = class{
     isOkay = task.getInfo().title == 'test1' && task.getInfo().isCompleted == true;
     console.log("test2", isOkay);
 });
-const Folder = class{
+const Folder = class extends Set{ // folder is Set
     constructor(title){
+        super();
         this.title = title;
-        this.tasks = new Set(); // task에는 중복 값이 있을 수 없다! addTask()에서 부가적인 중복값 확인 로직을 대신하여 Collection 사용
     }
-    // 1. 은닉 수준이 높은 design
-    // Folder 안에 있어야하므로 task에 대한 정보는 Folder만 알고 있으면 된다.
-    // 인자는 String만 넘기면 된다.
-    // (-) 폴더를 옮겨야할 때 객체를 생성해야한다
-    // 폴더 간 이동은 없도록 결정한 디자인!
-    // removeTask()와의 대칭성 위배!
-    // addTask(title){ 
-    //     this.tasks.add(new Task(title)); // (-) task 생성은 folder의 책임이 아니어야 한다.
-    // }
-    // 2. task
-    // 따라서, folder 입장에서 task의 노출은 피할 수 없다
-    // task와 folder의 decoupling
+    // 어떤 메소드가 어떤 객체에 들어가야 하는지 애매할 땐 아래와 같이 대입해보면 된다.
+    // ex - App은 task 정보를 알 필요가 없다. 의존성이 늘어나면 잘못된 위치!
+    moveTask(task, folderSrc, folderDest) {
+        if(super.has(task) || !folderSrc.has(task)) throw err('.');
+        folderSrc.removeTask(task);
+        this.addTask(task);
+    } // replace, move, remove & add
     addTask(task){
         if(!(task instanceof Task)) err('invalid task'); // 강타입
-        this.tasks.add(task); 
+        this.tasks.add(task);
+        super.add(task);
     }
     removeTask(task){
         if(!(task instanceof Task)) err('invalid task');
-        this.tasks.delete(task);
+        super.delete(task);
     }
-    getTasks(){
-        return [...this.tasks.values()];
+    getTasks(){ 
+        return [...super.values()];
     }
     getTitle(){
         return this.title;
     }
+    // 내적 동질성의 원리
+    add(){} delete(){err('...');} clear(){} values(){} 
 };
-(()=>{
-    let isOkay = true;
-    const task = new Task('test1');
-    const folder = new Folder('folder1');
-
-    isOkay = folder.getTasks().length == 0; 
-    console.log("test1", isOkay);
-    folder.addTask(task);
-    isOkay = folder.getTasks().length == 1 && folder.getTasks()[0].getInfo().title
-    == 'test1';
-    console.log("test2", isOkay);
-    folder.addTask(task);
-    isOkay = folder.getTasks().length == 1 && folder.getTasks()[0].getInfo().title
-    == 'test1';
-    console.log("test3", isOkay);
-});
 // 도메인 구조
-const App = class{
+const App = class extends Set{
+    static get(title) {return new Folder(title);} // Factory pattern
     constructor(){
-        this.folders = new Set(); // App이 알아야만 하는 것들만 저장. 중복 제거
+        super();
     }
     addFolder(folder){
         if(!(folder instanceof Folder)) err('invalid folder'); // 강타입
-        this.folders.add(folder); 
+        super.add(folder); 
     }
     removeFolder(folder){
         if(!(folder instanceof Folder)) err('invalid folder');
-        this.folders.delete(folder);
+        super.delete(folder);
     }
     getFolders(){
-        return [...this.folders.values()];
+        return [...super.values()];
     }
 };
-
 const Renderer = class{
     constructor(app){
         this.app = app;
@@ -116,41 +99,24 @@ const el = (tag)=>document.createElement(tag);
 const DOMRenderer = class extends Renderer{
     constructor(parent, app){
         super(app);
-        this.el = parent.appendChild(el('section'));
-        this.el.innerHTML = `
-            <nav>
-                <input type="text">
-                <ul></ul>
-            </nav>
-            <section>
-                <header>
-                    <h2></h2>
-                    <input type="text">
-                </header>
-                <ul></ul>
-            </section>
-        `;
-        this.el.querySelector('nav').style.cssText = 'float:left; width:200px; border-right:1px solid #000';
-        this.el.querySelector('section').style.cssText = 'margin-left:210px'
-        const ul = this.el.querySelectorAll('ul');
-        this.folder = ul[0];
-        this.task = ul[1];
+        const [folder, task] = Array.from(document.querySelectorAll('ul'));
+        this.folder = folder;
+        this.task = task;
         this.currentFolder = null;
-        const input = this.el.querySelectorAll('input');
-        input[0].addEventListener("keyup", e=>{
+        parent.querySelector('nav>input').addEventListener("keyup", e=>{
             if(e.keyCode != 13) return;
             const v = e.target.value.trim();
             if(!v) return; // 쉴드 패턴
-            const folder = new Folder(v);
+            const folder = Folder.get(v);  
             this.app.addFolder(folder);
             e.target.value = ''; // 필수!
             this.render();
         });
-        input[1].addEventListener("keyup", e=>{
+        parent.querySelector('header>input').addEventListener("keyup", e=>{
             if(e.keyCode != 13 || !this.currentFolder) return;
             const v = e.target.value.trim();
             if(!v) return; // 쉴드 패턴
-            const task = new Task(v);
+            const task = Task.get(v);
             this.currentFolder.addTask(task);
             e.target.value = ''; // 필수!
             this.render();
@@ -159,31 +125,74 @@ const DOMRenderer = class extends Renderer{
     _render(){
         // 데이터 원본
         const folders = this.app.getFolders();
+        let moveTask, tasks;
         if(!this.currentFolder) this.currentFolder = folders[0];
-        this.folder.innerHTML = '';
+        let oldEl = this.folder.firstElementChild, lastEl;
         folders.forEach(f=>{
-            const li = el('li');
+            let li;
+            if(oldEl) {
+                li = oldEl;
+                oldEl = oldEl.nextElementSibling;
+            } else {
+                li = el('li');
+                this.folder.appendChild(li);
+                oldEl = null;
+            }
+            lastEl = li;
             li.innerHTML = f.getTitle();
             li.style.cssText = 'font-weight:' + (this.currentFolder == f? 'bold' : 'normal');
-            li.addEventListener("click", ()=>{
+            li.onclick = ()=>{
                 this.currentFolder = f;
                 this.render();
-            });
-            this.folder.appendChild(li);
+            };
+            li.ondrop = e=>{
+                e.preventDefault();
+                f.moveTask(moveTask, this.currentFolder, t);
+            };
+            li.ondragover = e=>{
+                e.preventDefault();
+            };
         });
+        if(lastEl)
+        while(oldEl = lastEl.nextElementSibling) {
+            this.folder.removeChild(oldEl);
+        }
         if(!this.currentFolder) return;
-        this.task.innerHTML = '';
-        this.currentFolder.getTasks().forEach(t=>{
-            const li = el('li');
+        tasks = this.currentFolder.getTasks();
+        if(tasks.length == 0) {
+            while(oldEl == this.task.firstElementChild) {
+                this.task.removeChild(oldEl);
+            }
+        } else {
+            oldEl = this.task.firstElementChild, lastEl = null;
+            tasks.forEach(t=>{
+            let li;
+            if(oldEl) {
+                li = oldEl;
+                oldEl = oldEl.nextElementSibling;
+            } else {
+                li = this.taskEl.length? this.taskEl.pop() : el('li'); // 증분 렌더링 구현
+                this.task.appendChild(li);
+                oldEl = null;
+            }
+            lastEl = li;
             const {title, isCompleted} = t.getInfo();
+            li.setAttribute('draggable', true);
             li.innerHTML = (isCompleted? 'Completed ' : 'process ') + title;
-            li.addEventListener("click", ()=>{
+            li.addEventListener("click", e=>{
                 t.toggle();
                 this.render();
             });
-            this.task.appendChild(li);
+            li.addEventListener("dragstart", e=>{
+                moveTask = t;
+            });
+            if(lastEl)
+                while(oldEl == lastEl.nextElementSibling) {
+                    this.task.removeChild(oldEl);
+                    this.taskEl.push(oldEl);
+                }
         });
     }
 };
 
-new DOMRenderer(document.body, new App());
+new DOMRenderer(document.querySelector('main'), new App());
